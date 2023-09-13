e6cec26274782ed6517dacac3c6db3b546c96253
AghaDev <mohamadagha2@gmail.com>
2023-09-13 14:52:10 +0300

    comments

--- src/App.js
+++ src/App.js
@@ -7,24 +7,26 @@ import fakeWeatherData from "./fakeWeatherData.json";
 import "./App.css";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "alialahmad_MohammadAlAgha"
-    };
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     name: "alialahmad_MohammadAlAgha"
+  //   };
+  // }
 
-  handleInputChange = value => {
-    this.setState({ name: value });
-  };
+  // handleInputChange = value => {
+  //   this.setState({ name: value });
+  // };
 
   render() {
     return (
-      <div className="app">
-        <SayHi />
-        <SayHello color="black" name={this.state.name} />
-        <Search handleInput={this.handleInputChange} />
-      </div>
+      // <div className="app">
+      //   /* <SayHi />
+      //   <SayHello color="black" name={this.state.name} />
+      //   <Search handleInput={this.handleInputChange} /> */
+      // </div>
+
+      <div> alialahmad_MohammadAlAgha</div>
     );
   }
 }
