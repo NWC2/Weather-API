4b7f3b6680c2561cf52b0d6eb05dfa47847cf605
RayanAlHassan <rayanalhassan2000@gmail.com>
2023-09-13 13:55:52 +0300

    create responsive footer with kitten image

--- src/App.js
+++ src/App.js
@@ -21,51 +21,49 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <footer>
-          <section className="container">
-            <section className="sectionss one">
-              <span>03:00</span>
-              <img src="https://placekitten.com/200/287"></img>
-              <span>8'c</span>
-            </section>
-
-            <section className="sectionss two">
-              <span>06:00</span>
-              <img src="https://placekitten.com/200/286"></img>
-              <span>8'c</span>
-            </section>
-
-            <section className="sectionss three">
-              <span>09:00</span>
-              <img src="https://placekitten.com/200/140"></img>
-              <span>8'c</span>
-            </section>
-
-            <section className="sectionss four">
-              <span>12:00</span>
-              <img src="https://placekitten.com/200/138"></img>
-              <span>9'c</span>
-            </section>
-
-            <section className="sectionss five">
-              <span>14:00</span>
-              <img src="https://placekitten.com/200/139"></img>
-              <span>17'c</span>
-            </section>
-
-            <section className="sectionss six">
-              <span>18:00</span>
-              <img src="https://placekitten.com/200/141"></img>
-              <span>16'c</span>
-            </section>
-
-            <section className="sectionss seven">
-              <span>21:00</span>
-              <img src="https://placekitten.com/200/141"></img>
-              <span>13'c</span>
-            </section>
+        <section className="container">
+          <section className="hour one">
+            <span>03:00</span>
+            <img src="https://placekitten.com/200/287"></img>
+            <span>8'c</span>
           </section>
-        </footer>
+
+          <section className="hour two">
+            <span>06:00</span>
+            <img src="https://placekitten.com/200/286"></img>
+            <span>8'c</span>
+          </section>
+
+          <section className="hour three">
+            <span>09:00</span>
+            <img src="https://placekitten.com/200/140"></img>
+            <span>8'c</span>
+          </section>
+
+          <section className="hour four">
+            <span>12:00</span>
+            <img src="https://placekitten.com/200/138"></img>
+            <span>9'c</span>
+          </section>
+
+          <section className="hour five">
+            <span>14:00</span>
+            <img src="https://placekitten.com/200/139"></img>
+            <span>17'c</span>
+          </section>
+
+          <section className="hour six">
+            <span>18:00</span>
+            <img src="https://placekitten.com/200/141"></img>
+            <span>16'c</span>
+          </section>
+
+          <section className="hour seven">
+            <span>21:00</span>
+            <img src="https://placekitten.com/200/141"></img>
+            <span>13'c</span>
+          </section>
+        </section>
       </div>
     );
   }
