09953574308d95377a7b824a53ee238932db484a
RayanAlHassan <rayanalhassan2000@gmail.com>
2023-09-14 15:53:31 +0300

    Weather API: Extracting the values from the Fake Weather json

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from "react";
-import Search from "./components/Search";
 
-import { CurrentWeather, WeatherItem } from "./components/WeatherItem";
-import fakeWeatherData from "./fakeWeatherData.json";
-import clear from "./img/weather-icons/clear.svg";
-import cloudy from "./img/weather-icons/cloudy.svg";
-import partlycloudy from "./img/weather-icons/partlycloudy.svg";
-import mostlycloudy from "./img/weather-icons/mostlycloudy.svg";
 import Nav from "./components/Nav";
 import WeatherNow from "./components/WeatherNow";
 import HourlyWeather from "./components/HourlyWeather";
+import FakeWeather from "./fakeWeatherData.json";
 
 import "./App.css";
-
+const fake = FakeWeather
+console.log(fake)
+// console.log(FakeWeather)
 function App() {
   return (
     <div className="app">
       <Nav />
       <main>
-        <WeatherNow />
-        <HourlyWeather />
+        <WeatherNow  fake={fake}/>
+        <HourlyWeather fake = {fake}/>
       </main>
     </div>
   );
--- src/components/HourlyWeather.js
+++ src/components/HourlyWeather.js
@@ -3,93 +3,34 @@ import React from "react";
 import clear from "../img/weather-icons/clear.svg";
 import mostlycloudy from "../img/weather-icons/mostlycloudy.svg";
 
-function HourlyWeather() {
+function HourlyWeather({ fake }) {
+  {
+    fake.list.map((e) => {
+      console.log(e.dt_txt);
+    });
+  }
+
+  const getData = fake.list.slice(5, 11);
+
   return (
-    <section class="dayweather">
-      <article class="timedata">
-        <time class="time" datetime="hh:mm">
-          3:00
-        </time>
-        <img
-          class="weatherimg"
-          src={mostlycloudy}
-          alt="weatherimg"
-          placeholder="http://placekitten.com/g/200/300"
-        />
-        <span class="temp">8&deg;C </span>
-      </article>
-      <article class="timedata">
-        <time class="time" datetime="hh:mm">
-          6:00
-        </time>
-        <img
-          class="weatherimg"
-          src={mostlycloudy}
-          alt="weatherimg"
-          placeholder="http://placekitten.com/g/200/300"
-        />
-        <span class="temp">9&deg;C </span>
-      </article>
-      <article class="timedata">
-        <time class="time" datetime="hh:mm">
-          9:00
-        </time>
-        <img
-          class="weatherimg"
-          src={clear}
-          alt="weatherimg"
-          placeholder="http://placekitten.com/g/200/300"
-        />
-        <span class="temp">14&deg;C </span>
-      </article>
-      <article class="timedata">
-        <time class="time" datetime="hh:mm">
-          12:00
-        </time>
-        <img
-          class="weatherimg"
-          src={clear}
-          alt="weatherimg"
-          placeholder="http://placekitten.com/g/200/300"
-        />
-        <span class="temp">17&deg;C </span>
-      </article>
-      <article class="timedata">
-        <time class="time" datetime="hh:mm">
-          15:00
-        </time>
-        <img
-          class="weatherimg"
-          src={clear}
-          alt="weatherimg"
-          placeholder="http://placekitten.com/g/200/300"
-        />
-        <span class="temp">18&deg;C </span>
-      </article>
-      <article class="timedata">
-        <time class="time" datetime="hh:mm">
-          18:00
-        </time>
-        <img
-          class="weatherimg"
-          src={clear}
-          alt="weatherimg"
-          placeholder="http://placekitten.com/g/200/300"
-        />
-        <span class="temp">16&deg;C </span>
-      </article>
-      <article class="timedata">
-        <time class="time" datetime="hh:mm">
-          21:00
-        </time>
-        <img
-          class="weatherimg"
-          src={mostlycloudy}
-          alt="weatherimg"
-          placeholder="http://placekitten.com/g/200/300"
-        />
-        <span class="temp">13&deg;C </span>
-      </article>
+    <section className="dayweather">
+      {getData.map((e) => {
+        let hours = e.dt_txt.split(" ");
+        return (
+          <article className="timedetail">
+            <p className="time">{hours[1].slice(0, 5)}</p>
+            <img
+              className="wImg"
+              src={clear}
+              alt="weatherImag"
+              placeholder="http://placekitten.com/g/200/300"
+            />
+            <span className="temp">
+              {parseInt(e.main.temp - 273.15)}&deg;C{" "}
+            </span>
+          </article>
+        );
+      })}
     </section>
   );
 }
