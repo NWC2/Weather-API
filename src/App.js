6b45622be4746545614d8769c7f0e6906c93c049
JanaSaker <janasakr2000@gmail.com>
2023-09-14 14:38:38 +0300

    added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import SayHi, { SayHello } from "./components/WeatherItem";
 import fakeWeatherData from "./fakeWeatherData.json";
 import mostlyCloudy from '../src/img/weather-icons/mostlycloudy.svg';
 import "./App.css";
-
+import clear from '../src/img/weather-icons/clear.svg'
 
 class App extends Component {
   // constructor(props) {
@@ -27,17 +27,46 @@ class App extends Component {
         <input type="text" placeholder="Type in a city name"/>
         <button className="btn" onclick="">FIND WEATHER</button>
       </header>
-      <img src={mostlyCloudy}/>
+      <img className="firstimage" src={mostlyCloudy}/>
       <div className="overcast">
         overcast clouds
       </div>
-       <div className="temp">
-        Temperature
-      </div>
+      <p className="temp"><b>Temperature&nbsp;</b>&nbsp;10&deg; to 11&deg;C</p>
+      <p className="humidity"><b>Humidity &nbsp; </b>&nbsp;78% &nbsp;<b>    Pressure  &nbsp;</b>  100848</p>
       </main>
-      
+      <div className="table">
+        <table>
+        <tr>
+            <td>03:00</td>
+            <td>06:00</td>
+            <td>09:00</td>
+            <td>12:00</td>
+            <td>15:00</td>
+            <td>18:00</td>
+            <td>21:00</td>
+        </tr>
+        <tr className="secondrow">
+            <td><img src={mostlyCloudy}/></td>
+            <td><img src={mostlyCloudy}/></td>
+            <td><img src={clear}/></td>
+            <td><img src={clear}/></td>
+            <td><img src={clear}/></td>
+            <td><img src={clear}/></td>
+            <td><img src={mostlyCloudy}/></td>
+        </tr>
+        <tr>
+            <td>8&deg;C</td>
+            <td>9&deg;C</td>
+            <td>14&deg;C</td>
+            <td>17&deg;C</td>
+            <td>18&deg;C</td>
+            <td>16&deg;C</td>
+            <td>13&deg;C</td>
+        </tr>
+      </table>
       </div>
-    );
+      </div>
+      )
   }
 
 }
