6a71c560c3a06954d1b81043eeb58f9c650995c5
AghaDev <mohamadagha2@gmail.com>
2023-09-14 15:11:54 +0300

    Components Created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React, { Component } from "react";
 import mc from "./img/weather-icons/mostlycloudy.svg"
+import clear from "./img/weather-icons/clear.svg";
+import cloudy from "./img/weather-icons/cloudy.svg";
+import drizzle from "./img/weather-icons/drizzle.svg";
+import fog from "./img/weather-icons/fog.svg";
+import pc from "./img/weather-icons/partlycloudy.svg";
+import rain from "./img/weather-icons/rain.svg";
+import snow from "./img/weather-icons/snow.svg";
+import storm from "./img/weather-icons/storm.svg";
+import unknown from "./img/weather-icons/unknown.svg";
 import Search from "./components/Search";
-
+import Header from "./Header";
+import Main from "./Main";
+import Footer from "./Footer";
 import SayHi, { SayHello } from "./components/WeatherItem";
 import fakeWeatherData from "./fakeWeatherData.json";
 
@@ -28,28 +39,27 @@ class App extends Component {
 
       // <div> alialahmad_MohammadAlAgha</div>
         <div>
-              <header>
-      <input type="text" placeholder="Type in a city name" name="city-name" id="city-name" />
-        <button className="find-weather" onclick="" > FIND WEATHER</button>
-
-      </header>
-      <div className="img-mc">
-        <img src={mc} className="mostly-cloudy" />
-        <h2 className="mc-text">overcast clouds</h2>
-      </div>
-      <div className="temp-data">
-        <p><b>Temperature&nbsp;</b>&nbsp;10&deg; to 11&deg;C</p>
-      </div>
-      <div className="humidity-pressure">
-        <p className="humidity"><b>Humidity&nbsp;</b>&nbsp;&nbsp;&nbsp;&nbsp;78%</p>
-        <p className="pressure"><b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Pressure&nbsp;</b>&nbsp;&nbsp;&nbsp;&nbsp;1008.48</p>
-
-      </div>
+      <Header />
+      <Main />
+      <Footer />
+      
 
         </div>
       
-    );
+      );
+    }
   }
-}
 
 export default App;
+
+{/* <img src={mc}/>
+<p>09:00</p>
+<img src={mc}/>
+<p>12:00</p>
+<img src={mc}/>
+<p>15:00</p>
+<img src={mc}/>
+<p>18:00</p>
+<img src={mc}/>
+<p>21:00</p>
+<img src={mc}/> */}
