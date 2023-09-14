76fc36ffab6de9f795a24e7aadfab127946364ab
whathow <marwa@gmail.com>
2023-09-14 16:26:31 +0300

    added forecast

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import {useState} from 'react'
     import "./App.css";
 import mostlycloudy from "./img/weather-icons/mostlycloudy.svg"
-import clear from "./img/weather-icons/clear.svg"
+// import clear from "./img/weather-icons/clear.svg"
+import Forecast from './components/Forecast';
 function App() {
 
   
@@ -30,55 +31,9 @@ function App() {
           <p><span className="temperatureProperty">temperature</span> 10° to 11°C</p>
           <p><span className="humidityProperty">Humidity </span>78%<span className="pressureProperty"> Pressure </span>1008.4</p>
         </section>
+        </section>
+        <Forecast/>
 
-      </section>
-
-
-      <section className="weather-forecast">
-        <div className="time_1">
-          <time className="time" dateTime="9:00">09:00</time>
-          <img className="icon" src={clear} alt="Sunny" />
-          <p className="temperature">75°c</p>
-        </div>
-
-        <div className="time_2">
-          <time className="time" dateTime="6:00">06:00</time>
-          <img className="icon" src={clear} alt="Cloudy" />
-          <p className="temperature">68°c</p>
-        </div>
-
-        <div className="time_3">
-          <time className="time" dateTime="9:00">09:00</time>
-          <img className="icon" src={clear} alt="Cloudy" />
-          <p className="temperature">68°c</p>
-        </div>
-
-        <div className="time_4">
-          <time className="time" dateTime="12:00">12:00</time>
-          <img className="icon" src={clear} alt="Cloudy" />
-          <p className="temperature">68°c</p>
-        </div>
-
-        <div className="time_5">
-          <time className="time" dateTime="15:00">15:00</time>
-          <img className="icon" src={clear} alt="Cloudy" />
-          <p className="temperature">68°c</p>
-        </div>
-
-        <div className="time_6">
-          <time className="time" dateTime="18:00">18:00</time>
-          <img className="icon" src={clear} alt="Cloudy" />
-          <p className="temperature">68°c</p>
-
-        </div>
-
-        <div className="time_7">
-          <time className="time" dateTime="21:00">21:00</time>
-          <img className="icon" src={clear} alt="Cloudy" />
-          <p className="temperature">68°c</p>
-        </div>
-
-      </section>
 
     </main>
 
