06a5a3fa21e3f0f5253ab1fa6f1471c3375daee8
AbdallahSalami <AbdallahSalami@users.noreply.github.com>
2023-09-14 14:01:52 +0300

    Weather API: HTML/CSS

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Search from "./components/Search";
 
 import SayHi, { SayHello } from "./components/WeatherItem";
 import fakeWeatherData from "./fakeWeatherData.json";
-
+import storm from './img/weather-icons/storm.svg'
 import "./App.css";
 
 class App extends Component {
@@ -24,6 +24,11 @@ class App extends Component {
         <SayHi />
         <SayHello color="black" name={this.state.name} />
         <Search handleInput={this.handleInputChange} />
+
+        <img src={storm} alt="storm icon" />
+
+
+
       </div>
     );
   }
