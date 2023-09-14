b896ec530ffd460507b2760da8274884b36c42e2
alialahmad68 <ali.alahmad.cs@gmail.com>
2023-09-14 15:25:42 +0300

    updates

--- src/App.js
+++ src/App.js
@@ -10,16 +10,7 @@ import snow from "./img/weather-icons/snow.svg";
 import storm from "./img/weather-icons/storm.svg";
 import unknown from "./img/weather-icons/unknown.svg";
 import Search from "./components/Search";
-import clear from "./img/weather-icons/clear.svg";
-import cloudy from "./img/weather-icons/cloudy.svg";
-import drizzle from "./img/weather-icons/drizzle.svg";
-import fog from "./img/weather-icons/fog.svg";
-import pc from "./img/weather-icons/partlycloudy.svg";
-import mc from "./img/weather-icons/mostlycloudy.svg";
-import rain from "./img/weather-icons/rain.svg";
-import snow from "./img/weather-icons/snow.svg";
-import storm from "./img/weather-icons/storm.svg";
-import unknown from "./img/weather-icons/unknown.svg";
+
 
 
 import Header from "./Header";
